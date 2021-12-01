bfe02a2332c92d4a10f09b8b04cdcf8650a285ad
LinaPeralta <69905738+LinaPeralta@users.noreply.github.com>
2021-11-30 21:07:46 -0500

    se agragaron las demás pantallas

---
+++ app/src/main/java/com/example/appet/detallesProducto.java
@@ -0,0 +1,14 @@
+package com.example.appet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class detallesProducto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles_producto);
+    }
+}
