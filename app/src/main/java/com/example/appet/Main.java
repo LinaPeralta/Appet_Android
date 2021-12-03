208822e63e40a1c3d3e1c3a27213a100835e0a60
Natalia Betancourt <nataliabetancourtm@gmail.com>
2021-12-03 16:40:55 -0500

    Diseño main y cambio a pantalla de agregar productos

--- app/src/main/java/com/example/appet/Main.java
+++ app/src/main/java/com/example/appet/Main.java
@@ -2,13 +2,28 @@ package com.example.appet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Main extends AppCompatActivity {
+
+    private Button addPetBtn, addProBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Elements
+        addPetBtn = findViewById(R.id.addPetBtn);
+        addProBtn = findViewById(R.id.addProBtn);
+
+        //Button functions
+        addProBtn.setOnClickListener((v)->{
+            Intent addProduct = new Intent(this, ProductInfo.class);
+            startActivity(addProduct);
+        });
+
     }
 }
